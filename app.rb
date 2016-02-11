8c23afe60cf78d2d1b155de14e70f31a535016fc
Benjamin R. Wakefield <ben.wakefield@tnsatlanta.org>
2016-02-11 10:48:57 -0500

    Updated app.rb with quiz apparatus

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require("sinatra")
+require_relative("QA")
 
 enable :sessions
 
@@ -14,6 +15,36 @@ get("/game/:option")
 	if params["option"] == "new"
 
 		session["correct_answers"] = 0
-		@used = []
+		session["used"] = []
 	else 
-		session[""]
+		session["valid_choices"] = HOLIDAY.keys - session["used"]
+		session["currentq"] = session["valid_choices"].sample[0]
+		session["used"].push(session["currentq"])
+	end
+
+	@choices = []
+  @choices.push(session["currentq"])
+  while @choices.count < 5
+    new_choice = random_holiday
+    if @choices.include?(new_choice)
+    else
+      @choices.push(new_choice)
+    end
+  end
+  @choices = @choices.shuffle
+  erb :game
+
+
+end
+
+post("/submission") do
+  if params["guess"] == session["currentq"]
+    @correct = true
+    session["correct_answers"] += 1
+  else
+    @correct = false
+    session["missed"] += 1
+  end
+
+  erb :submission
+end

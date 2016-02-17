b7a284daede81ceba2b8ecb21ebb48f9d56580ca
Benjamin R. Wakefield <ben.wakefield@tnsatlanta.org>
2016-02-17 12:26:24 -0500

    Stuff\

---
+++ QA.rb
@@ -0,0 +1,44 @@
+HOLIDAY = {
+	"Christmas" => "This is a holiday in the christian calendar celebrating the birth of Jesus. 
+	It is often celebrated by non christians by giving gifts to family members and friends. Falls on December 25
+	
+
+	"New year's day" => "This holiday is celebrated in beginning of the year, there are often fireworks. Jan 1",
+
+	"Martin Luther King JR. day" => "This holiday celebrates the birthday of the leader of the Civil rights movement, 
+	which was a movement to get equal civil rights for all people in America. Jan 18",
+
+	"Valentine's day" => "A day where people celebrate their loved ones or significant others. Feb 14",
+
+	"Presidents' day" => "A day where we celebrate some of our best Presidents, George Washington and Abraham Linconln. Feb 15",
+
+	"Daylight savings time" => "Daylights Savings time is a time in between Spring and Fall where the clocks are pushed an hour back. 
+	It starts the second week of March and ends on the first week on November.",
+
+	"Saint Patrick's day" => "A holiday celebrating Irish tradition, 
+	often with parades and parties. March 17",
+
+	"Easter" => "A religious holiday celebrating the biblical resserection of Jesus. 
+	Outside of religious activities, there is also a tradition of activities like Egg hunting, or giving candy to one's family.",
+
+	"Mother's day" => "A day where people honor their Mothers. May 8",
+
+	"Memorial day" => "This holiday is in memory of past US soldiers. May 30",
+
+	"Father's day" => "A day in honor of Fathers. June 19",
+
+	"Independance day" => "A day celebrating the United States seperation from the Biritsh Empire in 1776. July 4th",
+
+	"Halloween" => "A holiday celebrated with scary decorations and themes. 
+	Children typically go out and 'trick-or-treat' from their neighbors, who hand out candy. October 31st",
+
+	"Election day" => "Depending on where you go to school you get this day off. 
+	This day in november is when US Citizens can vote.",
+
+	"Thanksgiving" => "A holiday in november based around a large meal, typically enjoyed with family members. ",
+
+	 }
+
+def random_holiday
+  HOLIDAY.to_a.sample[0]
+end
--- app.rb
+++ app.rb
@@ -6,38 +6,47 @@ enable :sessions
 
 get("/") do
 
-erb :index
+	erb :index
 
 end
 
-get("/quiz/:option")
+get("/quiz/:option") do
 
 	if params["option"] == "new"
 
 		session["correct_answers"] = 0
 		session["missed"] = 0
 		session["used"] = []
-	else 
-		session["valid_choices"] = HOLIDAY.keys - session["used"]
-		session["currentq"] = session["valid_choices"].sample[0]
+		session["valid_choices"] = HOLIDAY.keys
+		session["currentq"] = session["valid_choices"].to_a.sample
 		session["used"].push(session["currentq"])
+
+	elsif params["option"] == "next"
+
+		session["valid_choices"] = HOLIDAY.keys - session["used"]
+		session["currentq"] = session["valid_choices"].to_a.sample
+		session["used"].push(session["currentq"])
+
+	else
+
 
 	end
 
 	@choices = []
-  @choices.push(session["currentq"])
-  while @choices.count < 5
-    new_choice = random_holiday
-    if @choices.include?(new_choice)
-    else
-      @choices.push(new_choice)
-    end
-  end
+  	@choices.push(session["currentq"])
+  	while @choices.count < 5
+   	 	new_choice = random_holiday
+    	unless @choices.include?(new_choice)
+    		
+      		@choices.push(new_choice)
+    	end
+  	end
   @choices = @choices.shuffle
   erb :quiz
 
-
 end
+
+
 
 post("/submission") do
   if params["guess"] == session["currentq"]
